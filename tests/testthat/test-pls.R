caf53c88e466c8b05ecf9dfee6c2518bc2d918ad
Cyrus <86699936+cyrusae@users.noreply.github.com>
2023-04-02 14:09:02 -0700

    Unit test for URL scraping

--- tests/testthat/test-pls.R
+++ tests/testthat/test-pls.R
@@ -1,3 +1,23 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+#Using variables as of 2023.04.02:
+### Extract FY with prefix
+### IMLS site design stores years in the accordion
+test_that("PLS URL retrieval works", {
+  extract <- 'fy20..'
+  site <- 'https://www.imls.gov'
+  xpath <- '//*[@data-ui-role="accordion"]'
+  element <- 'a'
+  grepl <- '*pls_fy'
+  mock <- testthat::test_path('fixtures', 'PLS_sample.html') #path to the saved sample HTML file
+  snap <- testthat::test_path('fixtures',
+                              'PLS_URL_snapshot.txt') %>%
+    readLines() #saved example output (URLs only, no names)
+  pls <- get_pls_urls(url = mock,
+                      site = site,
+                      xpath = xpath,
+                      element = element,
+                      grepl = grepl,
+                      extract = extract)
+  names(snap) <- stringr::str_extract(snap, extract)
+  expect_equal(pls, snap)
 })
+
