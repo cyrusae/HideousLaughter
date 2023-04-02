03c35c62bef1827bde2d0782afd324b028d62df9
Cyrus <86699936+cyrusae@users.noreply.github.com>
2023-04-02 11:26:21 -0700

    Update utils.R

--- R/utils.R
+++ R/utils.R
@@ -11,6 +11,22 @@
 #' @export
 #'
 check_nrow <- \(file = filename) {
-  n <- data.table::fread(file = filename, select = 1L) %>%
+  assertthat::is.readable(file) #check filename
+  n <- data.table::fread(file = file, select = 1L) %>%
     nrow()
+}
+
+#' Vectorized `check_nrow()`
+#'
+#' @description
+#' Uses `data.table::fread()` to determine `nrow` for multiple files without actually loading their contents. Returns a vector of results in the original order.
+#'
+#' @param files Character vector of one or more file paths. (If it's only one, you should probably be using `check_nrow()` on its own.)
+#'
+#' @returns Returns an integer vector corresponding to the `nrow` results in order.
+#' @export
+#'
+check_nrows <- \(files = c()) {
+  assertthat::assert_that(is.vector(files, mode = 'character'))
+  ns <- purrr::map_int(.x = files, .f = check_nrow)
 }
