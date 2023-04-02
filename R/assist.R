f2c782439a7beeee48d9e09940ea9998ef58ef18
Cyrus <86699936+cyrusae@users.noreply.github.com>
2023-04-02 08:11:37 -0700

    Create assist.R

---
+++ R/assist.R
@@ -0,0 +1,2 @@
+# This file is for helper functionality for evaluating the data.
+# Nothing here should be needed for day-to-day use of the resulting ETL pipeline, but we might as well keep it around.
