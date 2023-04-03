dc74f64219723123441562ffee060d22d0c59a75
Cyrus <86699936+cyrusae@users.noreply.github.com>
2023-04-02 21:27:15 -0700

    Add assistants to-do list

--- R/assist.R
+++ R/assist.R
@@ -1,2 +1,7 @@
 # This file is for helper functionality for evaluating the data.
 # Nothing here should be needed for day-to-day use of the resulting ETL pipeline, but we might as well keep it around.
+
+
+### TODO: Add `lexicographer` function (roll over data to give me a high-level idea of what I'm dealing with)
+
+### TODO after that: Add `locksmith` function (roll over data to determine potentially relevant keys)
