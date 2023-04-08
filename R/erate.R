41b45ac7e163074f68ff28c55f007b867df999fe
Cyrus <86699936+cyrusae@users.noreply.github.com>
2023-04-07 17:23:43 -0700

    USAC url generator widget

--- R/erate.R
+++ R/erate.R
@@ -1,1 +1,31 @@
 # Functionality directly relating to downloading/sanitizing/filtering E-Rate data from USAC to make it bearable to deal with.
+
+#TODO: Fill out Roxygen skeletons
+#' Title
+#'
+#' @param dataset
+#' @param suffix
+#'
+#' @return
+#' @export
+#'
+#' @examples
+build_usac_url <- \(dataset, suffix = '.csv') {
+  prefix <- 'https://opendata.usac.org/resource/'
+  res <- paste0(prefix, dataset, suffix)
+}
+
+# TODO: wrapper that responds to human-readable names instead, because I can:
+#' Title
+#'
+#' @param name
+#' @param suffix
+#'
+#' @return
+#' @export
+#'
+#' @examples
+build_usac_url_named <- \(name, suffix = '.csv') {
+  # match name to dataset here
+  res <- build_usac_url(dataset = dataset, suffix = suffix)
+}
